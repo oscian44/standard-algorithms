604e24f3b616c59d32ed71bcc566242b5c018cfd
oscian <oscar@reinitz.com.au>
2022-03-08 14:26:11 +1100

    Framework HTML and started JS

---
+++ script/main.js
@@ -0,0 +1,45 @@
+var output = document.getElementById("output")
+
+function linearSearch(){
+    let myArray = [
+        "alpha",
+        "beta",
+        "gamma",
+        "delta",
+        "epsilon",
+        "zeta",
+        "eta",
+        "theta"
+    ]
+    
+
+
+}
+
+function findMaxandMin(){
+    let myArray = [
+        123,
+        2523,
+        39034,
+        27,
+        394,
+        56,
+        98,
+        12455,
+        1111,
+        239,
+        100,
+        43,
+        11
+    ]
+    let theMin
+    let theMax
+
+    for(i=1;i<myArray.length;i++){
+        if(myArray[i]<myArray[i-1]){
+            theMin = myArray[i]
+        }
+
+
+    }
+}
